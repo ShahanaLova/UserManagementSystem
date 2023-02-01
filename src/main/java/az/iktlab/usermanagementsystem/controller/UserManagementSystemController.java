e0a273b426db32f1a2e9c9f833d899f0ff939cda
LogmanovaSN <shahana.logmanova@kapitalbank.az>
2023-02-01 16:43:30 +0400

    Dto

--- src/main/java/az/iktlab/usermanagementsystem/controller/UserManagementSystemController.java
+++ src/main/java/az/iktlab/usermanagementsystem/controller/UserManagementSystemController.java
@@ -3,20 +3,40 @@ package az.iktlab.usermanagementsystem.controller;
 
 import az.iktlab.usermanagementsystem.model.User;
 import az.iktlab.usermanagementsystem.service.UserManagementSystemService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/user")
 public class UserManagementSystemController {
 
     private final UserManagementSystemService userService = new UserManagementSystemService();
 
-    public void save(User user){
-        userService.saveUser(user);
+    @PostMapping("/save")
+    public void save(@RequestBody User user) {
+         userService.saveUser(user);
     }
 
-    public List<User> getUsers(){
+    @GetMapping
+    public List<User> getUsers() {
         return userService.getUsers();
     }
 
-
+    @GetMapping("/id/{id}")
+    public User getUserById(@PathVariable("id") Long id){
+        return userService.getUserById(id);
+    }
+    @GetMapping("/fin")
+    public User getUserByFin(@RequestParam("fin") String fin){
+        return userService.getUserByFin( fin);
+    }
+    @DeleteMapping("/id")
+    public void deleteUserById(@RequestParam("id") Long id){
+        userService.deleteById(id);
+    }
+    @DeleteMapping
+    public void deleteAllUsers() {
+        userService.deleteAllUsers();
+    }
 }
--- src/main/java/az/iktlab/usermanagementsystem/service/UserManagementSystemService.java
+++ src/main/java/az/iktlab/usermanagementsystem/service/UserManagementSystemService.java
@@ -8,32 +8,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
-@RestController
-@RequestMapping("/user")
+
 public class UserManagementSystemService {
 
     private static final userManagementSystemRepository userRepo = new userManagementSystemRepository();
 
-    //private static final List<Person> p = new ArrayList<>();
-    //private static final User user = new User();
-    //private static final Person person = new Person();
-
-    @GetMapping
-    public List<User> getUsers() {
+    public  List<User> getUsers() {
         return userRepo.allUsers();
     }
 
-    @GetMapping("/id/{id}")
-    public User getUserById(@PathVariable("id") Long id) {
+
+    public User getUserById( Long id) {
         return userRepo.allUsers().stream()
-                .filter(u -> u.getId() == id)
+                .filter(u -> Objects.equals(u.getId(), id))
                 .findAny()
                 .orElseThrow(() -> {
                     throw new RuntimeException("User not found");
                 });
     }
 
-    @GetMapping("/fin")
+
     public User getUserByFin(@RequestParam("fin") String fin) {
         return userRepo.allUsers().stream()
                 .filter(u -> Objects.equals(u.getPerson().getFin(), fin))
@@ -44,17 +38,16 @@ public class UserManagementSystemService {
     }
 
 
-    @PostMapping("/save")
-    public void saveUser(@RequestBody User us) {
+    public void saveUser(User us) {
         userRepo.allUsers().add(us);
     }
 
-    @DeleteMapping("/id")
-    public void deleteById(@RequestParam("id") Long id) {
+
+    public void deleteById(Long id) {
         userRepo.allUsers().removeIf(u -> Objects.equals(u.getId(), id));
     }
 
-    @DeleteMapping
+
     public void deleteAllUsers() {
         userRepo.allUsers().clear();
     }
